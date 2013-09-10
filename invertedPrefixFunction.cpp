a41f5b83fa688470b41d933998ee1364f82a0eaa
Aleksandr Ostanin <alexostanin95@gmail.com>
2013-09-10 17:24:27 +0400

    inverted prefixFunction

---
+++ invertedPrefixFunction.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <time.h>
+#include <vector>
+#include <list>
+#include <cassert>
+#include <set>
+#include <map>
+#include <string>
+#include <queue>
+#include <cstdio>
+#include <cmath>
+#include <memory>
+using namespace std;
+
+typedef long long li;
+
+#define all(a) a.begin(), a.end()
+#define mp make_pair
+
+int main() {
+#ifdef PROGA
+	freopen("in.txt", "r", stdin);
+#endif
+	int n;
+	cin >> n;
+	vector<int> p(n);
+	for (int i = 0; i < n; ++i)
+		cin >> p[i];
+	string s;
+	s += 'a';
+	if (p[0] != 0) {
+		cout << "Impossible\n";
+		return 0;
+	}
+	for (int i = 1; i < n; ++i) {
+		if (p[i] > p[i - 1] + 1) {
+			cout << "Impossible\n";
+			return 0;
+		}
+		if (p[i] == 0) {
+			s += 'b';
+			continue;
+		}
+		int j = p[i - 1];
+		set<char> had;
+		while (j + 1 > p[i]) {
+			had.insert(s[j]);
+			j = p[j - 1];
+		}
+		if (j + 1 != p[i]) {
+			cout << "Impossible\n";
+			return 0;
+		}
+		if (had.find(s[j]) != had.end()) {
+			cout << "Impossible\n";
+			return 0;
+		}
+		s += s[j];
+	}
+	cout << s << "\n";
+}
+
+
+
+
+
+
